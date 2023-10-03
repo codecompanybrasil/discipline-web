a21adc83c895d12b8ae9553dce7c32b0af3b398e
Pablo Pereira <pablopereira27@gmail.com>
2023-10-03 16:58:57 -0300

    Incluída página de Detalhe da avaliação

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 
-import AvaliationListPage from "./Pages/AvaliationListPage";
 import LoadingPage from './Pages/LoadingPage';
+import AvaliationListPage from "./Pages/AvaliationListPage";
+import AvaliationDetailsPage from "./Pages/AvaliationDetailsPage";
 import AvaliationPage from "./Pages/AvaliationPage";
 
 export default createBrowserRouter([
@@ -19,6 +20,22 @@ export default createBrowserRouter([
         loader: async ({ request, params }) => {
             let data = await fetch(
                 `http://api.discipline.app.br/avaliations/${params.hash}/export`,
+                { signal: request.signal }
+            ).then(async (response: any) => {
+                if (response.ok) {
+                    return await response.json()
+                }
+            })
+
+            return data
+        },
+    },
+    {
+        path: "avaliacoes/:hash/detalhes",
+        element: <AvaliationDetailsPage />,
+        loader: async ({ request, params }) => {
+            let data = await fetch(
+                `http://api.discipline.app.br/avaliations/${params.hash}`,
                 { signal: request.signal }
             ).then(async (response: any) => {
                 if (response.ok) {
