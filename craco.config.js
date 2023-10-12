bf3c111c6bebe42f7d870a2c856f89181abdf18e
Pablo Pereira <pablopereira27@gmail.com>
2023-10-12 20:16:37 -0300

    FIX - Adicionado salt no contentHash do nome dos arquivos

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,17 @@
 const path = require('path');
+const { randomUUID } = require('crypto');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     webpack: {
         alias: {
             '@': path.resolve(__dirname, 'src/')
+        },
+        configure: (webpackConfig, { env, paths }) => {
+            webpackConfig.output.hashSalt = randomUUID()
+            return webpackConfig
         }
+
     },
     jest: {
         configure: {
