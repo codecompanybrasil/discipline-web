3e414caf9db09a17310a5520a0ae575a095aea05
Pablo Pereira <pablopereira27@gmail.com>
2023-11-11 16:25:52 -0300

    Inclusão de ADS
    Inclusão de ADS lateral para telas maiores que 1200px. Ajustes de layout para comportar essa inclusão.

--- src/Layouts/PageTemplate/index.tsx
+++ src/Layouts/PageTemplate/index.tsx
@@ -1,25 +1,42 @@
 import React from "react"
 import Header from "../Header"
 
-import styles from './layout.module.css';
+import './layout.css';
 
 const PageTemplate = ({ children }: React.PropsWithChildren) => (
-    <div className={["container", styles.layout_container].join(" ")}>{children}</div>
+    <div className={["container-fluid layout_container"].join(" ")}>{children}</div>
 )
 
 PageTemplate.Header = (props: any) => <Header title={props.title} />
 
 PageTemplate.Panel = (props: any) => {
-    const colSizes: string = "col-12 col-lg-10 offset-lg-1 col-xxl-8 offset-xxl-2 g-5 py-4"
-    
     return (
-        <div>  {/* Div extra necessária para fixar alterações do display grid. TODO: Revisitar essa área para entender melhor e fazer possiveis melhorias */}
-            <div className="row">
-                <div className={[styles.panel, colSizes].join(" ")}>
-                    <div className={styles.panel_inner}>
-                        {props.children}
+        <div className="content_container">  {/* Div extra necessária para fixar alterações do display grid. TODO: Revisitar essa área para entender melhor e fazer possiveis melhorias */}
+            <div className="menu d-none d-lg-block">
+                
+            </div>
+
+            <div className="container">
+                <div className="row">
+                    <div className="panel col-12 py-4">
+                        <div className="panel_inner">
+                            {props.children}
+                        </div>
                     </div>
                 </div>
+            </div>
+
+            <div className="ads d-none d-lg-block">
+                {/* <!-- Anúncio Lateral --> */}
+                <ins className="adsbygoogle"
+                    style={{ display: 'block' }}
+                    data-ad-client="ca-pub-3474364984949013"
+                    data-ad-slot="9484542151"
+                    data-ad-format="auto"
+                    data-full-width-responsive="true"></ins>
+                <script>
+                    (adsbygoogle = window.adsbygoogle || []).push({ });
+                </script>
             </div>
         </div>
     )
@@ -27,7 +44,7 @@ PageTemplate.Panel = (props: any) => {
 
 PageTemplate.Footer = (props: any) => {
     return (
-        <footer className={[styles.page_footer, "row"].join(" ")}>
+        <footer className="page_footer row">
             <div className="col-12">
                 {props.children}
             </div>
@@ -37,7 +54,7 @@ PageTemplate.Footer = (props: any) => {
 
 PageTemplate.DevInfo = () => {
     return (
-        <footer className={styles.dev_info}>
+        <footer className="dev_info">
             <div className="row gx-1 justify-content-center">
                 <div className="col-auto col-sm-auto">
                     <span className="copyright">© 2023 <a href="https://codecompany.org/" target="_blank">Code Company Brasil</a></span>
